6fb61af299d8230a521373cfb5076ac968f4b5b2
jose-roman-lo0 <jose.roman@fibercli.com>
2022-11-15 22:45:42 +0100

    Operators example

---
+++ session01/example10.py
@@ -0,0 +1,16 @@
+print(2 * 3)
+print(2 * 3.)
+print(2. * 3)
+print(2. * 3.)
+#versus
+print(6 / 3)
+print(6 / 3.)
+print(6. / 3)
+print(6. / 3.)
+#versus
+print(6 // 3)
+print(6 // 3.)
+print(6. // 3)
+print(6. // 3.)
+
+
