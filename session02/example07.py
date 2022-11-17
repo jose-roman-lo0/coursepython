7ce1bc73aae5b8a32aa2757b0b5c25834c2f36fb
jose-roman-lo0 <jose.roman@fibercli.com>
2022-11-17 14:33:33 +0100

    elif example

---
+++ session02/example07.py
@@ -0,0 +1,10 @@
+from art import *
+answer = input("Do you prefer airplane or car, for airplane use 1 for car use 2\n")
+# What is the problem with this structure?
+if (answer == "1"):
+    aprint("airplane1")
+# be carefull with else
+elif(answer == "2"):
+    aprint("car")
+else:
+    print("Maybe you like bike!!!")
