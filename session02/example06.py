14f4e66e52e24ff23ccfd9f356087fc390331106
jose-roman-lo0 <jose.roman@fibercli.com>
2022-11-17 14:20:33 +0100

    else

---
+++ session02/example06.py
@@ -0,0 +1,9 @@
+from art import *
+mujer = art("woman")
+acido = art("acid")
+answer = input("Do you prefer airplane or car, for airplane use 1 for car use 2\n")
+# What is the problem with this structure
+if (answer == "1"):
+    aprint("airplane1")
+else:
+    aprint("car")
