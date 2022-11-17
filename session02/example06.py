81bebe6a558a478b0180ec80141949f1bd3e8ca3
jose-roman-lo0 <jose.roman@fibercli.com>
2022-11-17 14:26:30 +0100

    else choice

--- session02/example06.py
+++ session02/example06.py
@@ -1,6 +1,4 @@
 from art import *
-mujer = art("woman")
-acido = art("acid")
 answer = input("Do you prefer airplane or car, for airplane use 1 for car use 2\n")
 # What is the problem with this structure?
 if (answer == "1"):
