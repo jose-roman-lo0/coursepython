a61a11a437d3bdba2b383dfaff8e005b372cf0d7
jose-roman-lo0 <jose.roman@fibercli.com>
2022-11-17 14:24:07 +0100

    else example

--- session02/example06.py
+++ session02/example06.py
@@ -5,5 +5,6 @@ answer = input("Do you prefer airplane or car, for airplane use 1 for car use 2\
 # What is the problem with this structure
 if (answer == "1"):
     aprint("airplane1")
+# be carefull with else
 else:
     aprint("car")
