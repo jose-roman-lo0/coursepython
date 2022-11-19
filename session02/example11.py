5716941b9d3d894b77145bbfdbf972e8ec0a3a6c
jose-roman-lo0 <jose.roman@fibercli.com>
2022-11-19 07:32:12 +0100

    For loop example 4

--- session02/example11.py
+++ session02/example11.py
@@ -1,3 +1,16 @@
+import time
 # Example for loop
 for i in range(20):
     print(i)
+
+# Using increment
+for i in range(2,20,2):
+    print(i)
+# Making some actions inside the loop
+x = 2
+for i in range(11):
+    print (x**i)
+
+for i in range(10):
+    print(i," waiting Ms Daysy")
+    time.sleep(1)
