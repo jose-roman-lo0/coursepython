cacb249984741a59d4a6f10f4f3205a02582d90f
jose-roman-lo0 <jose.roman@fibercli.com>
2022-11-20 08:30:28 +0100

    List methods example

--- session02/example15.py
+++ session02/example15.py
@@ -5,3 +5,11 @@ for i in range (1,10):
     my_list.append(2**i)
 
 print(my_list)
+
+my_list = []
+for i in range (1,10):
+    # we use insert method
+    my_list.insert(0,2**i)
+
+# Please check the difference between booth methods
+print(my_list)
