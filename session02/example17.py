40d6d0071dda964fb8e0c9c0e8d6004bf470fcee
jose-roman-lo0 <jose.roman@fibercli.com>
2022-11-20 09:00:21 +0100

    Reverse list programmatically and with a reverse method

--- session02/example17.py
+++ session02/example17.py
@@ -10,3 +10,8 @@ for i in range(1,((len(my_list)//2)+1)):
     my_list[i-1],my_list[-i] = my_list[-i],my_list[i-1]
 
 print(my_list)
+
+# You have a built in method to reverse a list!!!!!
+my_list.reverse()
+print(my_list)
+
