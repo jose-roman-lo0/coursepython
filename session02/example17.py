93e2f28bac16a4c870096d9f06a5e6e4a64ec2a7
jose-roman-lo0 <jose.roman@fibercli.com>
2022-11-20 08:53:51 +0100

    Example reversing a list iterating

---
+++ session02/example17.py
@@ -0,0 +1,12 @@
+# Example with index, swapping elements
+my_list = []
+# You can generate a list
+for i in range(6):
+    my_list.append(i**2)
+print(my_list)
+
+# We use negative index and list lenght
+for i in range(1,((len(my_list)//2)+1)):
+    my_list[i-1],my_list[-i] = my_list[-i],my_list[i-1]
+
+print(my_list)
