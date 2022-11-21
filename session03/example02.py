0a2442bfb8b67714337dfc771906d49eb6abe5f6
jose-roman-lo0 <jose.roman@fibercli.com>
2022-11-21 05:49:25 +0100

    Function parameter example

---
+++ session03/example02.py
@@ -0,0 +1,7 @@
+# Function parameter example
+def input_message(my_type):
+    print("Please introduce a ",my_type,":")
+    
+print("Main program")
+input_message("string")
+input_message("number")
