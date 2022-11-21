cd8fc85d76f78cc5f5e3213ef01050676d1d6427
jose-roman-lo0 <jose.roman@fibercli.com>
2022-11-21 06:09:31 +0100

    Parametrized function example

---
+++ session03/example04.py
@@ -0,0 +1,7 @@
+# Parametrized function example
+
+def message_intro(text="Introduce an", my_type="integer:"):
+    print(text,my_type)
+    
+message_intro()
+message_intro("Introduce a","string:")
