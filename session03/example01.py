cdbb81c1730de7bba2b63e501a061fe2f7052119
jose-roman-lo0 <jose.roman@fibercli.com>
2022-11-21 05:41:01 +0100

    Function example

---
+++ session03/example01.py
@@ -0,0 +1,15 @@
+# Function example
+def input_message():
+    print("Please introduce a",end=" ")
+
+print("Main program")
+input_message()
+print("integer:")
+my_var = input()
+print(my_var)
+
+# We reuse the function
+input_message()
+print("string:")
+my_var = input()
+print(my_var)
