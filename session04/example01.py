440b1e0a4c285e4d53de89e3f6187d1d7317ad89
jose-roman-lo0 <108176231+jose-roman-lo0@users.noreply.github.com>
2022-12-05 14:38:15 +0100

    Create example01.py

---
+++ session04/example01.py
@@ -0,0 +1,9 @@
+# define a function that returns None
+def my_function():
+  print("This function doesn't return anything")
+  return None
+# call the function and print its return value
+result = my_function()
+print(result)  # prints None
+
+
