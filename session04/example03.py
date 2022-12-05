5681a8fd162486523a5e5831c4337599fb3fde5e
jose-roman-lo0 <108176231+jose-roman-lo0@users.noreply.github.com>
2022-12-05 15:08:51 +0100

    Create example03.py

---
+++ session04/example03.py
@@ -0,0 +1,6 @@
+# define a function that returns a list
+def my_function():
+  my_list = [1, 2, 3]
+  return my_list
+# call the function and store its return value in a variable
+result = my_function()
