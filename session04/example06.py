fe92b73b9f2aff2b3c0e2a2a954b88d3a982b31a
jose-roman-lo0 <108176231+jose-roman-lo0@users.noreply.github.com>
2022-12-05 15:14:21 +0100

    Update example06.py

--- session04/example06.py
+++ session04/example06.py
@@ -9,3 +9,9 @@ my_tuple = (1, 2, 3)
 # access elements of the tuple by index
 print(my_tuple[0])  # prints 1
 
+# try to modify an element of the tuple
+my_tuple[0] = 5  # this will raise an error because tuples are immutable
+# tuples can be used in places where a list is expected
+numbers = [1, 2, 3]
+my_function(my_tuple)  # this is valid
+my_function(numbers)  # this is also valid
