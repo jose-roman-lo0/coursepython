eac502ad2224ff367aca632faefe08c6be5c080e
jose-roman-lo0 <108176231+jose-roman-lo0@users.noreply.github.com>
2022-12-05 15:16:37 +0100

    Create example07.py

---
+++ session04/example07.py
@@ -0,0 +1,15 @@
+# create a dictionary
+my_dict = {
+  "key1": "value1",
+  "key2": "value2",
+  "key3": "value3"
+}
+
+# access the value associated with a key
+print(my_dict["key1"])  # prints "value1"
+
+# add a new key-value pair to the dictionary
+my_dict["key4"] = "value4"
+
+# delete a key-value pair from the dictionary
+del my_dict["key2"]
