e6d95b79adb8b7df0042ec8a34a4e19a0b7d9fc8
jose-roman-lo0 <108176231+jose-roman-lo0@users.noreply.github.com>
2022-12-05 15:13:03 +0100

    Create example06.py

---
+++ session04/example06.py
@@ -0,0 +1,11 @@
+# create a tuple with three elements
+my_tuple = (1, 2, 3)
+
+# access elements of the tuple by index
+print(my_tuple[0])  # prints 1
+# create a tuple with three elements
+my_tuple = (1, 2, 3)
+
+# access elements of the tuple by index
+print(my_tuple[0])  # prints 1
+
