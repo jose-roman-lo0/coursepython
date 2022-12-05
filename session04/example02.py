2df74b178ac36596a35602a39d167c41df034b9f
jose-roman-lo0 <108176231+jose-roman-lo0@users.noreply.github.com>
2022-12-05 14:40:22 +0100

    Create example02.py

---
+++ session04/example02.py
@@ -0,0 +1,9 @@
+# define a function that takes a list as an argument
+def my_function(my_list):
+  # iterate over the elements of the list and print them
+  for item in my_list:
+    print(item)
+# create a list and pass it as an argument to the function
+my_list = [1, 2, 3]
+my_function(my_list)
+
